87eb1b26feb7f383a1058a402b97f3faeb67fec0
Lorekgaston <lorekgaston@gmail.com>
2021-03-31 00:04:50 -0300

    added new dependecies

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+app.use(helmet());
+
 app.use(cors());
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.use('/api/v1/users', userRouter);
 
